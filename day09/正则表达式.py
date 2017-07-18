ecf765a905a1ce3d08ad91beab4ad03153df0f5f
w1033834071 <www.1033834071@qq.com>
2017-07-18 21:38:14 +0800

    补充

--- day09/正则表达式.py
+++ day09/正则表达式.py
@@ -16,3 +16,25 @@ import re
 # r = re.findall('alex$','hello alex')
 # print(r)
 
+# * + ? { } 处理‘重复’的字符
+# ‘*’ 表示‘x’出现0次或多次  此处为‘贪婪匹配’
+# r = re.findall('alex*','alexxxxxxxx')
+# print(r)
+
+# ‘+’表示出现一次或多次
+# ‘？’表示出现0次或1次
+# {3,5}表示匹配3-5次都可以
+# [bc] 表示匹配到b或c  'bc'不能同时出现
+# [^1-9] 表示匹配‘非’1-9的其他字符
+
+# p = re.compile(r'\d+')
+# li = p.split('one1two2three3four4')
+# print(li)
+# 结果 ['one', 'two', 'three', 'four', '']
+
+ret = re.split('[bc]','abcd')
+print(ret)
+
+
+
+
