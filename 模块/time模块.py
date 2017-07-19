f6925296309bc85ce5b6500d0b26ce90286e5dc7
haveAdrink <2232339224@qq.com>
2017-07-19 21:05:00 +0800

    time模块

--- 模块/time模块.py
+++ 模块/time模块.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import time
+import datetime
 
 # 打印从1970年到现在的时间戳
 print(time.time())
@@ -9,5 +10,11 @@ print(time.time())
 # 换算一个time  可以选择填入一个时间
 print(time.ctime(time.time() - (60*60*24)))
 
+# 格林威治‘时间对象’
 time_obj = time.gmtime()
 print("%s-%s-%s %s:%s" % (time_obj.tm_year,time_obj.tm_mon,time_obj.tm_mday,time_obj.tm_hour,time_obj.tm_min))
+
+
+# 10天以前
+new_time = datetime.datetime.now() - datetime.timedelta(days=10)
+print(new_time)
